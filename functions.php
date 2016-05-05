10c738df167de7cfd3fade07e4d34f5f7c8708be
carm86 <carm_crh@hotmail.com>
2016-05-05 16:06:12 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -125,42 +125,6 @@ function team_post_type(){
 
 add_action('init', 'team_post_type', 0);
 
-
-/**
- * Register `department` taxonomy
- */
-function team_taxonomy() {
-
-    // Labels
-    $singular = 'Department';
-    $plural = 'Departments';
-    $labels = array(
-        'name' => _x( $plural, "taxonomy general name"),
-        'singular_name' => _x( $singular, "taxonomy singular name"),
-        'search_items' =>  __("Search $singular"),
-        'all_items' => __("All $singular"),
-        'parent_item' => __("Parent $singular"),
-        'parent_item_colon' => __("Parent $singular:"),
-        'edit_item' => __("Edit $singular"),
-        'update_item' => __("Update $singular"),
-        'add_new_item' => __("Add New $singular"),
-        'new_item_name' => __("New $singular Name"),
-    );
-
-    // Register and attach to 'team' post type
-    register_taxonomy( strtolower($singular), 'team', array(
-        'public' => true,
-        'show_ui' => true,
-        'show_in_nav_menus' => true,
-        'hierarchical' => true,
-        'query_var' => true,
-        'rewrite' => false,
-        'labels' => $labels
-    ) );
-}
-add_action( 'init', 'team_taxonomy', 0 );
-
-
 ?>
 
 
